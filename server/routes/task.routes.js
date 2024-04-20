9a8c32247cee03a78011d21f8591302a7333fef4
Abhinav Yadav <abhinavay2003@gmail.com>
2024-04-20 21:45:39 +0530

    Fix file extensions in task.routes.js

--- server/routes/task.routes.js
+++ server/routes/task.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { verifyJwt } from "../middlewares/verifyJWT";
-import { createTask, deleteTask, getAllTask, updateTask } from "../controllers/task.controllers";
+import { verifyJwt } from "../middlewares/verifyJWT.js";
+import { createTask, deleteTask, getAllTask, updateTask } from "../controllers/task.controllers.js";
 
 const router = Router();
 
 router.get("/getAllTask/:todoId", verifyJwt, getAllTask)
 router.post("/createTask/:todoId", verifyJwt, createTask);
-router.put("updateTask/:taskId", verifyJwt, updateTask);
-router.delete("deleteTask/:taskId", verifyJwt, deleteTask);
+router.put("/updateTask/:taskId", verifyJwt, updateTask);
+router.delete("/deleteTask/:taskId", verifyJwt, deleteTask);
 
 export default router;
