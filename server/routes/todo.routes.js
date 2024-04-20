0e63970589b9075a03dba99b6d48b039c0041051
Abhinav Yadav <abhinavay2003@gmail.com>
2024-04-20 19:54:17 +0530

    Refactor todo.routes.js: Update routes and import statements

--- server/routes/todo.routes.js
+++ server/routes/todo.routes.js
@@ -1,38 +1,13 @@
 import { Router } from "express";
-import { asyncHandler } from "../utils/asyncHandler.js";
-import ApiError from "../utils/ApiError.js";
-import Todo from "../models/todo.model.js";
-import ApiResponse from "../utils/ApiResponse.js";
-
+import { verifyJwt } from "../middlewares/verifyJWT.js";
+import { createTodo, deleteTodo, editTodo, getTodos } from "../controllers/todo.controllers.js";
 const router = Router();
 
-
-router.post("/createTodo", asyncHandler(async (req, res) => {
-    const { title } = req.body;
-    if (!title) {
-        throw new ApiError(400, "Title is required");
-    }
-
-    const user = req.user;
-    const todo = await Todo.create({ title, user: user._id });
-
-    console.log("Created Todo: " + todo);
-
-    if (!todo) {
-        throw new ApiError(500, "Failed to create todo");
-    }
-
-    return res.status(201).json({ data: todo, message: "Todo created successfully" });
-}));
-
+router.post("/createTodo", verifyJwt, createTodo);
+router.get("/todos", verifyJwt, getTodos);
+router.patch("/editTodo", verifyJwt, editTodo);
+router.delete("/deleteTodo", verifyJwt, deleteTodo);
 
 
 export default router;
 
-/**
- * User can do several things : 
- * + user can create the todo
- * + get the todo which the user created
- * + user can edit the todo title
- * + user can delete the todo
- */
